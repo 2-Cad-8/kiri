771a963674a6aded17a93db2b3a1d2a973a703bc
Carla <cad01hg@gmail.com>
2023-04-15 16:24:25 -0400

    modified:   css/estilos.css 	modified:   css/estilos.less 	new file:   js/swiper.js 	modified:   package-lock.json 	modified:   package.json 	modified:   profile.html 	new file:   profileOriginal.html

---
+++ js/swiper.js
@@ -0,0 +1,27 @@
+  // core version + navigation, pagination modules:
+  import Swiper from '../node_modules/swiper/swiper-bundle.esm.browser.js';
+
+  // import styles bundle
+  //import '../node_modules/swiper/swiper-bundle.css';
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    /*pagination: {
+      el: '.swiper-pagination',
+    },*/
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
